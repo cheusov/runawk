d9ba78361bb204d52c73cc9baf73e764d09a230c
Aleksey Cheusov <vle@gmx.net>
2007-09-22 10:51:49 +0000

    my own limited version of fgetln

--- fgetln.c
+++ fgetln.c
@@ -1,84 +1,27 @@
-/* modified by Aleksey Cheusov */
-
-/*	$NetBSD: fgetln.c,v 1.3 2004/09/11 19:01:40 jlam Exp $	*/
-
-/*-
- * Copyright (c) 1998 The NetBSD Foundation, Inc.
- * All rights reserved.
+/*
+ * Copyright (c) 2006 Aleksey Cheusov
  *
- * This code is derived from software contributed to The NetBSD Foundation
- * by Christos Zoulas.
+ * This material is provided "as is", with absolutely no warranty
+ * expressed or implied. Any use is at your own risk.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *        This product includes software developed by the NetBSD
- *        Foundation, Inc. and its contributors.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
+ * Permission to use or copy this software for any purpose is hereby
+ * granted without fee, provided the above notices are retained on all
+ * copies.  Permission to modify the code and to distribute modified
+ * code is granted, provided the above notices are retained, and a
+ * notice that the code was modified is included with the above
+ * copyright notice.
  *
- * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
- * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
  */
 
 char *
 fgetln(FILE *fp, size_t *len)
 {
-	static char *buf = NULL;
-	static size_t bufsiz = 0;
-	char *ptr;
+	static char buf [4096];
 
-	if (buf == NULL) {
-		bufsiz = BUFSIZ;
-		if ((buf = malloc(bufsiz)) == NULL)
-			return NULL;
-	}
-
-	if (fgets(buf, bufsiz, fp) == NULL)
+	if (fgets (buf, sizeof (buf), fp)){
+		*len = strlen (buf);
+		return buf;
+	}else{
 		return NULL;
-	*len = 0;
-
-	while ((ptr = strchr(&buf[*len], '\n')) == NULL) {
-		size_t nbufsiz = bufsiz + BUFSIZ;
-		char *nbuf = realloc(buf, nbufsiz);
-
-		if (nbuf == NULL) {
-			int oerrno = errno;
-			free(buf);
-			errno = oerrno;
-			buf = NULL;
-			return NULL;
-		} else
-			buf = nbuf;
-
-		/*
-		 * We need to overwrite the '\0' written by the last call
-		 * to fgets().
-		 */
-		*len = bufsiz - 1;
-		if (fgets(&buf[bufsiz - 1], BUFSIZ + 1, fp) == NULL)
-			return buf;
-
-		bufsiz = nbufsiz;
 	}
-
-	*len = (ptr - buf) + 1;
-	return buf;
 }
