ce6dc9a008cd110db35b86764f783c40efa7e4e9
Aleksey Cheusov <vle@gmx.net>
2007-09-22 12:02:54 +0000

    MIT licenced

--- runawk.c
+++ runawk.c
@@ -1,15 +1,14 @@
 /*
- * Copyright (c) 2006 Aleksey Cheusov
+ * Copyright (c) 2006, Aleksey Cheusov <vle@gmx.net>
  *
- * This material is provided "as is", with absolutely no warranty
- * expressed or implied. Any use is at your own risk.
- *
- * Permission to use or copy this software for any purpose is hereby
- * granted without fee, provided the above notices are retained on all
- * copies.  Permission to modify the code and to distribute modified
- * code is granted, provided the above notices are retained, and a
- * notice that the code was modified is included with the above
- * copyright notice.
+ * Permission to use, copy, modify, distribute and sell this software
+ * and its documentation for any purpose is hereby granted without
+ * fee, provided that the above copyright notice appear in all copies
+ * and that both that copyright notice and this permission notice
+ * appear in supporting documentation.  I make no
+ * representations about the suitability of this software for any
+ * purpose.  It is provided "as is" without express or implied
+ * warranty.
  *
  */
 
