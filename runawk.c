5f24cfbb19af7f5985f380b98be0d2ca3bf5b336
Aleksey Cheusov <vle@gmx.net>
2007-09-22 13:04:02 +0000

    year 2007

--- runawk.c
+++ runawk.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006, Aleksey Cheusov <vle@gmx.net>
+ * Copyright (c) 2007, Aleksey Cheusov <vle@gmx.net>
  *
  * Permission to use, copy, modify, distribute and sell this software
  * and its documentation for any purpose is hereby granted without
