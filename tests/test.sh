2bd3446c17a5f0209654d17a4fd74c240bf15395
Aleksey Cheusov <vle@gmx.net>
2009-04-01 20:54:04 +0000

    Yet another interesting demo for tokenre.awk module

--- tests/test.sh
+++ tests/test.sh
@@ -133,7 +133,8 @@ export AWKPATH=`pwd`/../modules
 runtest ../examples/demo_multisub
 
 ####################    tokenre
-runtest ../examples/demo_tokenre ../examples/demo_tokenre.in
+runtest ../examples/demo_tokenre  ../examples/demo_tokenre.in
+runtest ../examples/demo_tokenre2 ../examples/demo_tokenre2.in
 
 ####################    getopt
 runtest ../examples/demo_alt_getopt -h -
