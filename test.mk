1b533b0ef4e7fc944bf76811348a42482f9e3ec1
Aleksey Cheusov <vle@gmx.net>
2009-09-11 22:38:42 +0000

    move tests to test.mk

---
+++ test.mk
@@ -0,0 +1,28 @@
+.PHONY : test
+test : runawk
+	@echo 'running tests...'; \
+	export OBJDIR=${.OBJDIR}; \
+	if cd ${.CURDIR}/tests && ./test.sh > ${.OBJDIR}/_test.res && \
+	    diff -u test.out ${.OBJDIR}/_test.res; \
+	then echo '   succeeded'; \
+	else echo '   failed'; false; \
+	fi
+
+# test using all available awk version except mawk which
+# is definitely buggy, oawk is also NOT supported
+
+AWK_PROGS=/usr/bin/awk /usr/bin/nawk /usr/bin/gawk /usr/bin/original-awk \
+   /usr/pkg/bin/nawk /usr/pkg/bin/gawk /usr/pkg/bin/nbawk #\
+#   /usr/pkg/heirloom/bin/posix/awk /usr/pkg/heirloom/bin/posix2001/awk
+#   /usr/pkg/heirloom/bin/nawk
+#   /usr/bin/mawk /usr/pkg/bin/mawk /usr/pkg/bin/mawk-uxre
+
+.PHONY: test_all
+test_all:
+.for awk in ${AWK_PROGS}
+	@if test -x ${awk}; then \
+		echo testing ${awk}; \
+		export RUNAWK_AWKPROG=${awk}; cd ${.CURDIR} && ${MAKE} test; \
+		echo ''; \
+	fi
+.endfor
