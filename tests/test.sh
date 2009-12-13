1e1159766e1d104ba5111d8f7d3eb08ed2d0b110
Aleksey Cheusov <vle@gmx.net>
2009-12-13 21:41:57 +0000

    fix in regression test

--- tests/test.sh
+++ tests/test.sh
@@ -11,7 +11,7 @@ OBJDIR=${OBJDIR:=..}
 SRCDIR=`pwd`/..
 
 unify_paths (){
-    sed -e "s,`pwd`,ROOT," \
+    sed -e "s,/.*/tests,ROOT," \
 	-e 's,/tmp/runawk[.]......,/tmp/runawk.NNNNNN,' \
 	-e 's,new_argv \[0\] = .*awk.*,new_argv [0] = awk,' \
 	-e 's,ARGV\[0\]=.*awk.*,ARGV[0]=awk,' \
